eb3e12e71335359b06c454bb3e421d0818b41ba4
Jihyun0511 <yjhjulia1@gmail.com>
2022-07-02 20:07:00 +0900

    main scene made

---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    //눌렀을 때(OnClick) Stage Scene 으로 이동(Load Scene)
+    public void OnClickMoveToStageScene()
+    {
+        SceneManager.Instance.ChangeScene(SceneManager.ENUM_SCENE.Stage);
+        Debug.Log("Stage Scene 으로 이동");
+    }
+}
