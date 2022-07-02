079530cc2cfc76220feb4808f7e9f34891f3d5dd
ParkSolHwi <psh50zmfhtm@gmail.com>
2022-07-02 23:54:36 +0900

    [솔휘] 캐릭터 클래스 생성
    .

--- Assets/Scripts/Character/Enemy/EnemyUnit.cs
+++ Assets/Scripts/Character/Enemy/EnemyUnit.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// 우에서 좌로 움직이는 유닛, 아군 유닛을 공격하지 않으며, 타워를 향해 움직임
 /// </summary>
 
-public class EnemyUnit : ActiveObject
+public abstract class EnemyUnit : ActiveObject
 {
 	
 }
--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -3,8 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+public class MonsterData
+{
+	public string name;
+	public int hpMax;
+	public int damage;
+	public float moveSpeed;
+	public bool isAnimation;
+	public int stage;
+}
+
+public class BossData
+{
+	public string name;
+	public int hpMax;
+	public int damage;
+	public float moveSpeed;
+	public float attackSpeed;
+	public float spawnTime;
+}
+
+public class StageData
+{
+	public List<float> spawnTimeList = new List<float>();
+}
+
 public class ResourceManager : Singleton<ResourceManager>
 {
+	public bool IsReadyResource
+	{
+		get;
+		private set;
+	} = false;
+
 	private readonly string URL = "";
 	private DownloadHandler dataHandler = null;
 
